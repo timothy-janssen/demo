4e11c9c9b02e63fe3f145bab0ec9218410d581cb
Timothy Janssen <timothy.janssen@sap.com>
2019-08-23 09:00:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ app.get('/', function (req, res) {
     res.end(`
         <!doctype html>
         <html>
+         
+        <body>
          <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
             channelId="c4e05563-1c31-4402-a4af-36cfb449edc4"
             token="3e67ada0d49cec0657911fe48d16f8f9"
             id="cai-webchat"
-            ></script>
-        <body>
-           
+            ></script>  
         </body>
         </html>
     `);

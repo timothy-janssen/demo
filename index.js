f41b952f8d92c04e94d6eb5efdafcfb3fb1973b4
Timothy Janssen <timothy.janssen@sap.com>
2019-09-16 14:04:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
         <!doctype html>
         <html>
 
-        <body style="background-image:url(./background.png)">
+        <body style="background-image:url(./background.png); background-repeat: no-repeat;background-size: cover;">
          <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
             channelId="58e7d997-157a-4587-822e-eb2fdc7ca5a5"
             token="1ebc7f310ce25d84f564dfd944ed4b08"

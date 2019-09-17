3da3c07249c472f7aa0e27c01ad92b373c01ab2b
Timothy Janssen <timothy.janssen@sap.com>
2019-09-17 10:23:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
         <html>
 
         <body style="background-image:url(./background.png); background-repeat: no-repeat;background-size: cover;">
-        <p style="text-align:center;">Your challenge: Find 10 sentences I don’t understand</p>
+        <b style="text-align:center; color:white; ">Your challenge: Find 10 sentences I don't understand</b>
          <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
             channelId="58e7d997-157a-4587-822e-eb2fdc7ca5a5"
             token="1ebc7f310ce25d84f564dfd944ed4b08"

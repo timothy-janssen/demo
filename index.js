c0477f13269ef0a4f8951f00cfe41d37eb3b74a9
Timothy Janssen <timothy.janssen@sap.com>
2019-10-14 17:16:49 -0700

    add sfsf demo

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
                         color:white; 
                         background-color:red; 
                         font-size: 25px;
-                        padding-top: 5px;
+                        padding-top: 5px;   
                         padding-bottom: 5px;
                         padding-right: 10px;
                         padding-left: 10px;
@@ -52,6 +52,25 @@ app.get('/chatbot-breakfast', function (req, res) {
         <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
             channelId="833f1886-7c9b-496d-9952-eaf46d92f942"
             token="a9d2b226a26742f83f44b52f9a8efb61"
+            id="cai-webchat">
+        </script>
+    </body>
+</html>
+    `);
+});
+
+
+app.get('/chatbot-breakfast', function (req, res) {
+
+    res.end(`
+<!doctype html>
+<html>
+
+    <body style="background-image:url(./sfsf_background.png); background-repeat: no-repeat;background-size: cover;">
+         
+        <script src="https://cdn.cai.tools.sap/webchat/webchat.js"
+            channelId="14740ab0-1097-40d0-bd6d-e44fe2053f55"
+            token="9d6b5233cb253552e3eee3201f4fcfa4"
             id="cai-webchat">
         </script>
     </body>
